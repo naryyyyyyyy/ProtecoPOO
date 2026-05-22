56cccd144ad1aadbbea365fe27425fe2989b8ff4
CetinaPlus <cetigaming03@gmail.com>
2026-05-21 20:24:03 -0600

    asdasd

---
+++ CarreraDeCaballos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bj
+{
+    public class CarreraDeCaballos : Juego
+    {
+        public override void IniciarJuego()
+        {
+            base.IniciarJuego();
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bj
+{
+    public class Usuario
+    {
+        string Nombre {  get; set; }
+        int Id { get; set; }
+        private string Contrasena { get; set; }
+        double Saldo { get; set; }
+        public Usuario(string nombre, int id, string contrasena, double saldo)
+        {
+            Nombre = nombre;
+            Id = id;
+            Contrasena = contrasena;
+            Saldo = saldo;
+        }
+    }
+}
