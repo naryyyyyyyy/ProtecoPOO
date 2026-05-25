48350ac44b85a1684372a9671d3c1ee5c5bdb71c
Emiliano Naranjo Jimenez <a24214883@alumnos.uady.mx>
2026-05-25 17:17:24 -0600

    se agrego jugo id a registro y se corrigio el @juegoid

--- Usuario.cs
+++ Usuario.cs
@@ -11,9 +11,11 @@ namespace ProtecoPOO
         public string Nombre { get; set; }
         public int Id { get; set; }
         private string Contrasena { get; set; }
+        private int Personajeid {  get; set; }
         public decimal Saldo { get; set; }
-        public Usuario(string nombre, int id, string contrasena, decimal saldo)
+        public Usuario(string nombre, int id,int personajeid, string contrasena, decimal saldo)
         {
+            Personajeid = personajeid;
             Nombre = nombre;
             Id = id;
             Contrasena = contrasena;
