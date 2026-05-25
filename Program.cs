039227feff0bf61d9802c0fd23aabbba18b0715f
Emiliano Naranjo Jimenez <a24214883@alumnos.uady.mx>
2026-05-24 23:01:15 -0600

    listo por ahora

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ProtecoPOO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CarreraCaballos());
+            Application.Run(new Form1());
         }
     }
 }
