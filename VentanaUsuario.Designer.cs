7ef0f8b6c214bf9f132ac5aab657212f400c1473
Tabita Sarai Arias Sanchez <a24214890@alumnos.uady.mx>
2026-05-26 07:56:09 -0600

    Ventana usuario faltan datos

--- VentanaUsuario.Designer.cs
+++ VentanaUsuario.Designer.cs
@@ -31,14 +31,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VentanaUsuario));
             this.btnblackjack = new System.Windows.Forms.Button();
             this.btnCarreraCaballos = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnblackjack
             // 
-            this.btnblackjack.Location = new System.Drawing.Point(479, 265);
+            this.btnblackjack.Location = new System.Drawing.Point(479, 264);
             this.btnblackjack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnblackjack.Name = "btnblackjack";
-            this.btnblackjack.Size = new System.Drawing.Size(112, 35);
+            this.btnblackjack.Size = new System.Drawing.Size(129, 44);
             this.btnblackjack.TabIndex = 0;
             this.btnblackjack.Text = "blackjack";
             this.btnblackjack.UseVisualStyleBackColor = true;
@@ -46,7 +50,7 @@
             // 
             // btnCarreraCaballos
             // 
-            this.btnCarreraCaballos.Location = new System.Drawing.Point(479, 332);
+            this.btnCarreraCaballos.Location = new System.Drawing.Point(479, 355);
             this.btnCarreraCaballos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCarreraCaballos.Name = "btnCarreraCaballos";
             this.btnCarreraCaballos.Size = new System.Drawing.Size(129, 62);
@@ -55,6 +59,38 @@
             this.btnCarreraCaballos.UseVisualStyleBackColor = true;
             this.btnCarreraCaballos.Click += new System.EventHandler(this.btnCarreraCaballos_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 289);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(285, 160);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mi perfil";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Papyrus", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(484, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 51);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jugar";
+            // 
             // VentanaUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -62,12 +98,17 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(748, 543);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnCarreraCaballos);
             this.Controls.Add(this.btnblackjack);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "VentanaUsuario";
             this.Text = "VentanaUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +116,8 @@
 
         private System.Windows.Forms.Button btnblackjack;
         private System.Windows.Forms.Button btnCarreraCaballos;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
