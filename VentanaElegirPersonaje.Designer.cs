438459326136a282e971e83592d6233cd5c04634
Tabita Sarai Arias Sanchez <a24214890@alumnos.uady.mx>
2026-05-25 22:33:48 -0600

    Listo ventana elige a tu personaje

--- VentanaElegirPersonaje.Designer.cs
+++ VentanaElegirPersonaje.Designer.cs
@@ -28,11 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VentanaElegirPersonaje));
             this.panelContenedor = new System.Windows.Forms.Panel();
-            this.btnCaballero = new System.Windows.Forms.Button();
-            this.btnPicaro = new System.Windows.Forms.Button();
             this.btnMago = new System.Windows.Forms.Button();
+            this.btnPicaro = new System.Windows.Forms.Button();
+            this.btnCaballero = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelContenedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panelContenedor
@@ -40,53 +48,123 @@
             this.panelContenedor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelContenedor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelContenedor.BackgroundImage")));
+            this.panelContenedor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelContenedor.Controls.Add(this.label1);
+            this.panelContenedor.Controls.Add(this.pictureBox3);
+            this.panelContenedor.Controls.Add(this.pictureBox2);
+            this.panelContenedor.Controls.Add(this.pictureBox1);
             this.panelContenedor.Controls.Add(this.btnMago);
             this.panelContenedor.Controls.Add(this.btnPicaro);
             this.panelContenedor.Controls.Add(this.btnCaballero);
-            this.panelContenedor.Location = new System.Drawing.Point(-4, -3);
+            this.panelContenedor.Location = new System.Drawing.Point(-4, -4);
+            this.panelContenedor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelContenedor.Name = "panelContenedor";
-            this.panelContenedor.Size = new System.Drawing.Size(807, 456);
+            this.panelContenedor.Size = new System.Drawing.Size(908, 570);
             this.panelContenedor.TabIndex = 0;
-            // 
-            // btnCaballero
-            // 
-            this.btnCaballero.Location = new System.Drawing.Point(369, 356);
-            this.btnCaballero.Name = "btnCaballero";
-            this.btnCaballero.Size = new System.Drawing.Size(75, 23);
-            this.btnCaballero.TabIndex = 0;
-            this.btnCaballero.Text = "Caballero";
-            this.btnCaballero.UseVisualStyleBackColor = true;
-            this.btnCaballero.Click += new System.EventHandler(this.btnCaballero_Click);
-            // 
-            // btnPicaro
-            // 
-            this.btnPicaro.Location = new System.Drawing.Point(571, 356);
-            this.btnPicaro.Name = "btnPicaro";
-            this.btnPicaro.Size = new System.Drawing.Size(75, 23);
-            this.btnPicaro.TabIndex = 0;
-            this.btnPicaro.Text = "Picaro";
-            this.btnPicaro.UseVisualStyleBackColor = true;
-            this.btnPicaro.Click += new System.EventHandler(this.btnPicaro_Click);
             // 
             // btnMago
             // 
-            this.btnMago.Location = new System.Drawing.Point(181, 356);
+            this.btnMago.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnMago.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMago.Location = new System.Drawing.Point(117, 463);
+            this.btnMago.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnMago.Name = "btnMago";
-            this.btnMago.Size = new System.Drawing.Size(75, 23);
+            this.btnMago.Size = new System.Drawing.Size(128, 48);
             this.btnMago.TabIndex = 0;
             this.btnMago.Text = "Mago";
             this.btnMago.UseVisualStyleBackColor = true;
             this.btnMago.Click += new System.EventHandler(this.btnMago_Click);
             // 
+            // btnPicaro
+            // 
+            this.btnPicaro.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPicaro.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPicaro.Location = new System.Drawing.Point(640, 463);
+            this.btnPicaro.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPicaro.Name = "btnPicaro";
+            this.btnPicaro.Size = new System.Drawing.Size(116, 48);
+            this.btnPicaro.TabIndex = 0;
+            this.btnPicaro.Text = "Picaro";
+            this.btnPicaro.UseVisualStyleBackColor = true;
+            this.btnPicaro.Click += new System.EventHandler(this.btnPicaro_Click);
+            // 
+            // btnCaballero
+            // 
+            this.btnCaballero.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCaballero.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaballero.Location = new System.Drawing.Point(374, 463);
+            this.btnCaballero.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCaballero.Name = "btnCaballero";
+            this.btnCaballero.Size = new System.Drawing.Size(159, 48);
+            this.btnCaballero.TabIndex = 0;
+            this.btnCaballero.Text = "Bárbaro";
+            this.btnCaballero.UseVisualStyleBackColor = true;
+            this.btnCaballero.Click += new System.EventHandler(this.btnCaballero_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(67, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 286);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(323, 144);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(246, 282);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(600, 146);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(210, 280);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(131, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(647, 93);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Elige a tu personaje";
+            // 
             // VentanaElegirPersonaje
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.panelContenedor);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "VentanaElegirPersonaje";
             this.Text = "VentanaElegirPersonaje";
             this.panelContenedor.ResumeLayout(false);
+            this.panelContenedor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -97,5 +175,9 @@
         private System.Windows.Forms.Button btnMago;
         private System.Windows.Forms.Button btnPicaro;
         private System.Windows.Forms.Button btnCaballero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
