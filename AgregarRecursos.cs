42b825cce755c8413ed8ce2731694e3ffaf093fe
EDGAR DAVID PÉREZ TZEC <a21201005@alumnos.uady.mx>
2026-05-27 23:16:45 -0600

    cmbios a mi rama Ignoren

--- AgregarRecursos.cs
+++ AgregarRecursos.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿
+using ProtecoPOO.CasinoSQL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,24 +15,43 @@ namespace ProtecoPOO
 {
     public partial class AgregarRecursos : Form
     {
+        private string cadenaConexion;//conn
+        new ReportesDB reporte;
+        int idPlayer = SesionGlobal.UsuarioId;
+        int  idPlayerPers=SesionGlobal.PersonajeGuardadoId;
+        double saldoNu = SesionGlobal.SaldoActual;
+        string name = SesionGlobal.NombreUsuario;
+
         /// <summary>
         /// se tiene que pasar el objeto usuario a esta ventana para qu ese pueda guardar con el id
         /// </summary>
         public AgregarRecursos()
         {
             InitializeComponent();
+
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (!decimal.TryParse(txtAbono.Text, out decimal monto) || monto <= 0)
+            if (!double.TryParse(txtAbono.Text, out double monto) || monto <= 0)
             {
-                MessageBox.Show("Por favor, ingresa un monto de apuesta válido.", "Atención");
+                MessageBox.Show("Por favor, ingresa un monto de abono válido.", "Atención");
                 return;
             }
-            decimal NuevoMoto = monto/2;// cada creido vale 2 pesos 
-            //usuariodb=>modificar mnonto
-            //metodo para modificar el monto con el ususario y el id 
+            else
+            {
+                if (saldoNu == 0)
+                {
+                    saldoNu=1000;
+                    //usuariosDB.Modificarsaldo(saldoNu, SesionGlobal.UsuarioId, SesionGlobal.PersonajeGuardadoId);
+
+                    MessageBox.Show($"Fondos Agregados a {name} con personaje {idPlayerPers}. Ahora tu saldo es de: {saldoNu}.", "Atención");
+                }
+                MessageBox.Show($"Fondos No agregados a {name}. Se detecto credito aun vigente", "Atención");
+                return;
+            }
+
             
         }
 
